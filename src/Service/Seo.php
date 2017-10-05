12fa160d09a150ea849c15c7b8dc022d6f86fb86
YoBoby <julien.jovis@gmail.com>
2017-10-05 10:02:45 +0200

    get description

--- src/Service/Seo.php
+++ src/Service/Seo.php
@@ -14,6 +14,8 @@ class Seo extends BaseSeo
 {
   protected $em;
   protected $requestStack;
+  protected $seo;
+  protected $route;
 
     /**
     * @param EntityManagerInterface $em
@@ -24,6 +26,19 @@ class Seo extends BaseSeo
       parent::__construct();
       $this->em = $em;
       $this->requestStack = $requestStack;
+
+      $request = $this->requestStack->getCurrentRequest();
+
+      if( null !== ($this->route = $request->get('_route')) ){
+        $repository = $this->em->getRepository(SeoEntity::class);
+        $this->seo = $repository->createQueryBuilder('s')
+        ->join('s.routing', 'r')
+        ->where('r.route IN (:routename)')
+        ->setParameter('routename', $this->route)
+        ->getQuery()
+        ->getOneOrNullResult();
+
+      }
     }
 
     /**
@@ -34,20 +49,26 @@ class Seo extends BaseSeo
     */
     public function getTitle($default = null)
     {
-      $request = $this->requestStack->getCurrentRequest();
-      if( null !== ($route = $request->get('_route')) ){
-        $repository = $this->em->getRepository(SeoEntity::class);
-        $seo = $repository->createQueryBuilder('s')
-        ->join('s.routing', 'r')
-        ->where('r.route IN (:routename)')
-        ->setParameter('routename', $route)
-        ->getQuery()
-        ->getOneOrNullResult();
 
-        if( null !== $seo ){
-          return $seo->getTitle();
+        if( null !== $this->seo ){
+          return $this->seo->getTitle();
         }
-      }
+        return $default;
+    }
+
+    /**
+    *
+    * @param string $default
+    *
+    * @return string
+    */
+    public function getDescription($default = null)
+    {
+
+        if( null !== $this->seo ){
+          return $this->seo->getDescription();
+        }
+        return $default;
     }
 
 }
